d26778b33f01ed9d02ac93e33e3df7b9ffe06c6d
Carolina <krito_j_g@utp.edu.co>
2016-02-10 22:39:22 -0500

    se modificó el código, por el momento solo dibuja el plano cartesiano, ya no dibuja el vector

--- pantalla01.py
+++ pantalla01.py
@@ -11,31 +11,38 @@ AZUL = (0,0,255)
 
 class V:#vector
     def __init__(self,pf): #recibe los puntos como si el centro fuera 0,0
-        pi = [0,0] #punto inicial de todo vector
+        self.pi = [0,0] #punto inicial de todo vector
         self.pf = pf
+
+        #puntos iniciales trasladados
         x1 = pi[0] + CENTRO[0]
-        x2 = pi[1] + CENTRO[1]
-        y1 = pf[0] - CENTRO[0]
+        y1 = pi[1] + CENTRO[1]
+        self.pi_ = [x1,y1]
+
+        #puntos finales trasladados
+        x2 = pf[0] + CENTRO[0]
         y2 = pf[1] - CENTRO[1]
-        x = self.pi[1] - self.pi[0]
-        y = self.pf[1] - self.pf[0]
-        mag = math.sqrt(x*x + y*y) #magnitud
-        if x == 0 :
-            dire = 0
-        else:
-            dire = math.tan(y/x) #direccion
+        self.pf_ = [x2,y2]
 
     def setInitialPoint(self, pi):
         self.pi = pi
+        #puntos iniciales trasladados
+        x1 = pi[0] + CENTRO[0]
+        y1 = pi[1] + CENTRO[1]
+        self.pi_ = [x1,y1]
 
     def setFinalPoint(self, pf):
         self.pf = pf
+        #puntos finales trasladados
+        x = pf[0] + CENTRO[0]
+        y = pf[1] - CENTRO[1]
+        self.pf_ = [x2,y2]
 
     def getInitialPoint(self):
-        return pi
+        return self.pi_
 
     def getFinalPoint(self):
-        return pf
+        return self.pf_
 
 
 class L: #linea
@@ -54,13 +61,13 @@ class L: #linea
         self.pf = pf
 
     def getInitialPoint(self):
-        return pi
+        return self.pi
 
     def getFinalPoint(self):
-        return pf
+        return self.pf
 
 def imprime(o, c, a): #objeto, color, ancho
-    pygame.draw.line(pantalla, c, c.getInitialPoint(), c.getFinalPoint(), a)
+    pygame.draw.line(pantalla, c, o.getInitialPoint(), o.getFinalPoint(), a)
     pygame.display.flip() #actualizar la pantalla, funcion de refresco
 
 def sumaVec(v1, v2) :
@@ -80,8 +87,8 @@ pygame.init()
 
 pantalla = pygame.display.set_mode([ANCHO,ALTO])
 
-pini = [ANCHO,ALTO/2] #ancho, alto
-pfin = [0,ALTO/2]
+pfin = [ANCHO,ALTO/2] #ancho, alto
+pini = [0,ALTO/2]
 
 pi = [ANCHO/2, 0]
 pf = [ANCHO/2, ALTO]
@@ -100,18 +107,22 @@ p1 = [50, 50]
 p2 = [-50, -50]
 
 #construimos el vector
-v1 = V([0,0], p1)
-v2 = V([0,0], p2)
+v1 = V(p1)
+v2 = V(p2)
+
+print v1.getInitialPoint()
+print v1.getFinalPoint()
 
 #imprimimos el vector
-imprime(v1, VERDE, 3)
-
+#imprime(v1, VERDE, 3)
+#imprime(v2, VERDE, 3)
+'''
 #suma de vectores
 v3 = sumaVec(v1, v2)
 
 imprime(v3, VERDE, 3)
 
-
+'''
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
