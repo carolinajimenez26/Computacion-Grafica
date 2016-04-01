47b325ddd8c5ce95659b25c6535d07343c4f39f4
Carolina Jiménez Gómez <krito_j_g@utp.edu.co>
2016-04-01 12:17:49 -0500

    Ya funciona la función que dibuja el pixel

--- pixel.py
+++ pixel.py
@@ -1,4 +1,3 @@
-
 import pygame
 import sys
 import libreria1
@@ -11,11 +10,12 @@ pantalla = pygame.display.set_mode([ANCHO,ALTO])
 
 for i in range(0,100):
     #libreria1.DrawPixel([i,i], VERDE)
-    pantalla.set_at([i,i],VERDE)
-    libreria1.makeCircle([i,i],1,VERDE)
+    pantalla.set_at((i,i),VERDE)
+    #libreria1.makeCircle([i,i],1,VERDE)
 
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
+        pygame.display.flip()
