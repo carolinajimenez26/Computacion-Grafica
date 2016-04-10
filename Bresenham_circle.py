115972cb217cb48ad2762d227878cd7b622418da
Carolina <krito_j_g@utp.edu.co>
2016-04-10 16:37:10 -0500

    No grafica bien cuando el centro es diferente a 0,0

--- Bresenham_circle.py
+++ Bresenham_circle.py
@@ -10,34 +10,38 @@ BLANCO = (255,255,255)
 NEGRO = (0,0,0)
 
 def BresenhamCircle(xc,yc,r):#xcentro, ycentro, radio
-    x = 0
-    y = r
-    p = 1 - r
+    #iniciamos en 0,r
+    x = 0 + xc
+    y = r + yc
+    #p = 1 - r
+    p = 2*(x + 1)*(x + 1) + y*y + (y-1)*(y-1) - 2*r*r
     p_new = [[xc,yc],[x,y]]
     libreria1.DrawPixel(libreria1.Transform(p_new[0]), VERDE)
     libreria1.DrawPixel(libreria1.Transform(p_new[1]), VERDE)
     #se cicla hasta trazar todo un octante
     while (x < y) :
-        x = x + 1
-        if (p < 0) :
-            p = p + 2*x + 1
-        else :
+        x = x + 1 #siempre incrementamos en unidades enteras a x
+        if (p < 0) : #pintamos xi+1,yi
+            p = p + 4*x + 10 #segun mi ecuacion
+            #p = p + 4*x*x + 12*x + 4*y + 10
+        else : #pintamos xi+1,yi-1
             y = y - 1
-            p = p + 2*(x - y) + 1
+            p = p + 4*(x - y) + 10
+            #p = p + y*y + 4*x - 5*y + 8
 
-        libreria1.DrawPixel(libreria1.Transform([x,y]), VERDE)
-        libreria1.DrawPixel(libreria1.Transform([-x,y]), VERDE)
-        libreria1.DrawPixel(libreria1.Transform([x,-y]), VERDE)
-        libreria1.DrawPixel(libreria1.Transform([-x,-y]), VERDE)
-        libreria1.DrawPixel(libreria1.Transform([y,x]), VERDE)
-        libreria1.DrawPixel(libreria1.Transform([-y,x]), VERDE)
-        libreria1.DrawPixel(libreria1.Transform([y,-x]), VERDE)
-        libreria1.DrawPixel(libreria1.Transform([-y,-x]), VERDE)
+        libreria1.DrawPixel(libreria1.Transform([x + xc,y + yc]), VERDE)
+        libreria1.DrawPixel(libreria1.Transform([-x + xc,y + yc]), VERDE)
+        libreria1.DrawPixel(libreria1.Transform([x + xc,-y + yc]), VERDE)
+        libreria1.DrawPixel(libreria1.Transform([-x + xc,-y + yc]), VERDE)
+        libreria1.DrawPixel(libreria1.Transform([y + xc,x + yc]), VERDE)
+        libreria1.DrawPixel(libreria1.Transform([-y + xc,x + yc]), VERDE)
+        libreria1.DrawPixel(libreria1.Transform([y + xc,-x + yc]), VERDE)
+        libreria1.DrawPixel(libreria1.Transform([-y + xc,-x + yc]), VERDE)
 
 
 libreria1.makePlane()
 
-BresenhamCircle(0,0,100)
+BresenhamCircle(10,10,100)
 
 while True:
     for event in pygame.event.get():
